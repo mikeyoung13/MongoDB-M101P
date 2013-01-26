80cc58828d92fcf956c499330c3a6825a788e6f8
Michael Young <mikeyoung13@gmail.com>
2013-01-26 18:01:23 -0500

    Python loops

---
+++ Week1/5_loop.py
@@ -0,0 +1,9 @@
+fruit = ['apple','orange','grape']
+new_fruit = []
+
+for item in fruit:
+    print item
+    new_fruit.append(item)
+
+print new_fruit
+
---
+++ Week1/6_loop_quiz.py
@@ -0,0 +1,6 @@
+sum = 0
+numbers = [1,2,3,5,8]
+for i in numbers:
+    sum = sum + i
+print i
+
