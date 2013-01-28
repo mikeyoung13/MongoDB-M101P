56813218e4b60a524efa321d9910d29fe7be0b40
Michael Young <mikeyoung13@gmail.com>
2013-01-27 22:09:30 -0500

    add cookie to bottle

--- week1/14_bottle_view.py
+++ week1/14_bottle_view.py
@@ -17,6 +17,12 @@ def favorite_fruit():
     if (fruit == None or fruit == ""):
         fruit="No Fruit Selected"
 
+    bottle.response.set_cookie("fruit",fruit)
+    bottle.redirect("/show_fruit")
+
+@bottle.route('/show_fruit')
+def show_fruit():
+    fruit = bottle.request.get_cookie("fruit")
     return bottle.template('14_fruit_selected.tpl', {'fruit':fruit})
 
 bottle.debug(True)
