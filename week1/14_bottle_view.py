630d0a6d958a47d78049167608519f515e91936b
Michael Young <mikeyoung13@gmail.com>
2013-01-27 21:39:50 -0500

    pass dict to bottle template

--- week1/14_bottle_view.py
+++ week1/14_bottle_view.py
@@ -3,7 +3,14 @@ import bottle
 @bottle.route('/')
 def home_page():
     mythings = ['apple','orange','banana','peach']
-    return bottle.template('14_bottle_view', username="Mike", things=mythings)
+
+    # one way:
+    #return bottle.template('14_bottle_view', username="Mike", things=mythings)
+
+    # another way:
+    return bottle.template('14_bottle_view',{'username':'Mike2',
+                                             'things' : mythings})
+
 
 bottle.debug(True)
 bottle.run(host='localhost',port=8080)
