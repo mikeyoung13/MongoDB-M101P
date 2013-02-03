6d5959b0a57d837191092457670c9282c7a36be1
Michael Young <mikeyoung13@gmail.com>
2013-02-03 16:00:40 -0500

    hw 2.3 complete

--- week2/hw2.3/user.py
+++ week2/hw2.3/user.py
@@ -75,6 +75,7 @@ def validate_login(connection, username, password, user_record):
 
         # YOUR WORK HERE XXX
         # user = (just a suggestion)
+        user = users.find_one({"_id":username})
 
         # END OF STUDENT WORK
     except:
@@ -171,6 +172,7 @@ def newuser(connection, username, password, email):
         # XXX YOUR WORK HERE
         print "about to insert a user"
         # users.SOMETHING     (just a suggestion)
+        users.insert(user)
 
     except pymongo.errors.OperationFailure:
         print "oops, mongo error"
